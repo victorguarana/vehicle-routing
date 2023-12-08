e9c7c2db512d6331854d4892890ace0c62ccf8c3
Victor Guaraná <victorguarana@hotmail.com>
2023-12-08 01:18:33 -0300

    implement simple drone insertion fix main complement gitignore

--- main.go
+++ main.go
@@ -1,20 +1,29 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/victorguarana/go-vehicle-route/src/gps"
 	"github.com/victorguarana/go-vehicle-route/src/greedy"
+	"github.com/victorguarana/go-vehicle-route/src/routes"
 	"github.com/victorguarana/go-vehicle-route/src/vehicles"
 )
 
 func main() {
-	p := gps.Point{}
-	car1 := vehicles.NewCar("vehicle 1", &p)
+	p := gps.GetMap().Deposits[0]
+	car1 := vehicles.NewCar("vehicle 1", p)
 	car1.NewDrone("drone 1")
+	car1.NewDrone("drone 2")
+	route1, _ := routes.NewRoute(car1)
 
-	_, err := greedy.ClosestNeighbor(car1, gps.GetMap())
+	err := greedy.ClosestNeighbor(route1, gps.GetMap())
 	if err != nil {
 		panic(err)
 	}
 
-	// fmt.Print(route.String())
+	fmt.Printf("route1: %s\n", route1)
+
+	greedy.DroneSimpleInsertion(route1)
+
+	fmt.Print(route1.String())
 }
