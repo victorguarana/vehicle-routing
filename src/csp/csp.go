ebb1cf2a2d3183f511c05c23431e99d9ecf87e14
Victor Guaraná <victorguarana@hotmail.com>
2024-05-17 19:34:08 -0300

    Implement CSP

---
+++ src/csp/csp.go
@@ -0,0 +1,51 @@
+package csp
+
+import (
+	"github.com/victorguarana/go-vehicle-route/src/gps"
+	"github.com/victorguarana/go-vehicle-route/src/itinerary"
+	"github.com/victorguarana/go-vehicle-route/src/slc"
+)
+
+func CoveringWithDrones(itineraryList []itinerary.Itinerary, m gps.Map, neighborhoodDistance float64) {
+	clientsNeighborhood := gps.MapNeighborhood(m.Clients, neighborhoodDistance)
+	for itineraryIndex := 0; len(clientsNeighborhood) > 0; itineraryIndex++ {
+		itn := slc.CircularSelection(itineraryList, itineraryIndex)
+		actualClient := gps.ClosestPointWithMostNeighbors(itn.ActualCarPoint(), clientsNeighborhood)
+		itn.MoveCar(actualClient)
+		deliverNeighborsWithDrones(itn, clientsNeighborhood[actualClient])
+		removeClientAndItsNeighborsFromMap(actualClient, clientsNeighborhood)
+		gps.RemovePointFromNearbyMap(actualClient, clientsNeighborhood)
+	}
+}
+
+// TODO: Implement an function similar to this one
+// but finish when drone can not support next client
+func deliverNeighborsWithDrones(itn itinerary.Itinerary, neighbors []gps.Point) {
+	if len(neighbors) <= 0 {
+		return
+	}
+	droneNumbers := itn.DroneNumbers()
+	actualCarPoint := itn.ActualCarPoint()
+	actualCarStop := itn.ActualCarStop()
+	// Need to set PackageSize to 0 to avoid drone support
+	actualCarPoint.PackageSize = 0
+	for neighborIndex, droneIndex := 0, 0; neighborIndex < len(neighbors); droneIndex++ {
+		actualNeighbor := neighbors[neighborIndex]
+		actualDroneNumber := slc.CircularSelection(droneNumbers, droneIndex)
+		if itn.DroneSupport(actualDroneNumber, actualNeighbor, actualCarPoint) {
+			itn.MoveDrone(actualDroneNumber, actualNeighbor)
+			neighborIndex++
+		} else {
+			itn.LandDrone(actualDroneNumber, actualCarStop)
+		}
+	}
+	itn.LandAllDrones(actualCarStop)
+}
+
+func removeClientAndItsNeighborsFromMap(client gps.Point, clientsNeighborhood gps.Neighborhood) {
+	neighbors := clientsNeighborhood[client]
+	for _, neighbor := range neighbors {
+		gps.RemovePointFromNearbyMap(neighbor, clientsNeighborhood)
+	}
+	gps.RemovePointFromNearbyMap(client, clientsNeighborhood)
+}
---
+++ src/csp/csp_suite_test.go
@@ -0,0 +1,13 @@
+package csp_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func TestCsp(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Csp Suite")
+}
