b390575c9069cdcef8ff5db6c41b5b9e27fcd58b
Victor Guaraná <victorguarana@hotmail.com>
2024-05-17 12:27:38 -0300

    improve itinerary and add tests

--- src/itinerary/itinerary.go
+++ src/itinerary/itinerary.go
@@ -7,6 +7,8 @@ import (
 	"github.com/victorguarana/go-vehicle-route/src/vehicles"
 )
 
+var flightFactory = routes.NewSubRoute
+
 // TODO: Hide its implementation from other packages tests
 // Avoid 'var mockedDrone1 = itinerary.DroneNumber(1)'
 type DroneNumber int
@@ -35,44 +37,35 @@ type itinerary struct {
 	car              vehicles.ICar
 	route            routes.IMainRoute
 	dronesAndFlights map[DroneNumber]subItinerary
-	flightFactory    func(routes.IMainStop) routes.ISubRoute
 }
 
-func New(car vehicles.ICar, initialPoint gps.Point) Itinerary {
+func New(car vehicles.ICar) Itinerary {
 	return itinerary{
 		car:              car,
 		dronesAndFlights: generateDronesAndFlights(car.Drones()),
-		flightFactory:    routes.NewSubRoute,
-		route:            routes.NewMainRoute(routes.NewMainStop(initialPoint)),
+		route:            routes.NewMainRoute(routes.NewMainStop(car.ActualPoint())),
 	}
 }
 
 func (i itinerary) ActualCarPoint() gps.Point {
-	return i.route.Last().Point()
+	return i.car.ActualPoint()
 }
 
 func (i itinerary) CarSupport(nextPoints ...gps.Point) bool {
-	route := append([]gps.Point{i.ActualCarPoint()}, nextPoints...)
-	return i.car.Support(route...)
+	return i.car.Support(nextPoints...)
 }
 
 func (i itinerary) DroneCanReach(droneNumber DroneNumber, nextPoints ...gps.Point) bool {
-	subItn := i.dronesAndFlights[droneNumber]
-	if flight := subItn.flight; flight != nil {
-		route := append([]gps.Point{flight.Last().Point()}, nextPoints...)
-		return subItn.drone.CanReach(route...)
-	}
-
-	route := append([]gps.Point{i.ActualCarPoint()}, nextPoints...)
-	return subItn.drone.CanReach(route...)
+	drone := i.dronesAndFlights[droneNumber].drone
+	return drone.CanReach(nextPoints...)
 }
 
 func (i itinerary) DroneNumbers() []DroneNumber {
-	drones := make([]DroneNumber, len(i.dronesAndFlights))
+	var droneNumbers []DroneNumber
 	for drone := range i.dronesAndFlights {
-		drones = append(drones, drone)
+		droneNumbers = append(droneNumbers, drone)
 	}
-	return drones
+	return droneNumbers
 }
 
 func (i itinerary) DroneIsFlying(droneNumber DroneNumber) bool {
@@ -82,20 +75,14 @@ func (i itinerary) DroneIsFlying(droneNumber DroneNumber) bool {
 
 func (i itinerary) DroneSupport(droneNumber DroneNumber, nextPoints ...gps.Point) bool {
 	subItn := i.dronesAndFlights[droneNumber]
-	if flight := subItn.flight; flight != nil {
-		route := append([]gps.Point{flight.Last().Point()}, nextPoints...)
-		return subItn.drone.Support(route...)
-	}
-
-	route := append([]gps.Point{i.ActualCarPoint()}, nextPoints...)
-	return subItn.drone.Support(route...)
+	return subItn.drone.Support(nextPoints...)
 }
 
 func (i itinerary) LandDrone(droneNumber DroneNumber, destination routes.IMainStop) {
 	subItn := i.dronesAndFlights[droneNumber]
 	if flight := subItn.flight; flight != nil {
 		flight.Return(destination)
-		subItn.drone.Land()
+		subItn.drone.Land(destination.Point())
 		subItn.flight = nil
 		i.dronesAndFlights[droneNumber] = subItn
 	}
@@ -105,7 +92,7 @@ func (i itinerary) LandAllDrones(landingStop routes.IMainStop) {
 	for droneNumber, subItn := range i.dronesAndFlights {
 		if flight := subItn.flight; flight != nil {
 			flight.Return(landingStop)
-			subItn.drone.Land()
+			subItn.drone.Land(landingStop.Point())
 			subItn.flight = nil
 			i.dronesAndFlights[droneNumber] = subItn
 		}
@@ -114,19 +101,20 @@ func (i itinerary) LandAllDrones(landingStop routes.IMainStop) {
 
 func (i itinerary) MoveCar(destination gps.Point) {
 	i.route.Append(routes.NewMainStop(destination))
+	i.car.Move(destination)
 }
 
 func (i itinerary) MoveDrone(droneNumber DroneNumber, destination gps.Point) {
 	subItn := i.dronesAndFlights[droneNumber]
 	if flight := subItn.flight; flight != nil {
 		flight.Append(routes.NewSubStop(destination))
-		subItn.drone.Move(flight.Last().Point(), destination)
+		subItn.drone.Move(destination)
 		return
 	}
 
-	subItn.flight = i.flightFactory(i.route.Last())
+	subItn.flight = flightFactory(i.route.Last())
 	subItn.flight.Append(routes.NewSubStop(destination))
-	subItn.drone.Move(i.ActualCarPoint(), destination)
+	subItn.drone.Move(destination)
 	i.dronesAndFlights[droneNumber] = subItn
 }
 
