ba2182dd78e5d621d02df0dd4e378da9975f8d35
Victor Guaraná <victorguarana@hotmail.com>
2024-05-23 00:40:23 -0300

    Add covering with increased drones functionality

--- main.go
+++ main.go
@@ -20,6 +20,7 @@ func main() {
 	ClosestNeighborWithDrones()
 
 	Covering()
+	CoveringMaxDrones()
 }
 
 func ClosestNeighbor() {
@@ -30,8 +31,10 @@ func ClosestNeighbor() {
 
 	totalDistance := measure.TotalDistance(itn)
 	totalTime := measure.TimeSpent(itn)
+	totalFuelSpent := measure.SpentFuel(itn)
 	log.Println("ClosestNeighbor: Total Distance:", totalDistance)
 	log.Println("ClosestNeighbor: Total Time:", totalTime)
+	log.Println("ClosestNeighbor: Total Fuel Spent:", totalFuelSpent)
 
 	filename := "closest-neighbor.png"
 	output.ToImage(filename, itn, totalDistance, totalTime)
@@ -47,8 +50,10 @@ func ClosestNeighborWithDrones() {
 
 	totalDistance := measure.TotalDistance(itn)
 	totalTime := measure.TimeSpent(itn)
+	totalFuelSpent := measure.SpentFuel(itn)
 	log.Println("ClosestNeighborWithDrones: Total Distance:", totalDistance)
 	log.Println("ClosestNeighborWithDrones: Total Time:", totalTime)
+	log.Println("ClosestNeighborWithDrones: Total Fuel Spent:", totalFuelSpent)
 
 	filename := "closest-neighbor-with-drones.png"
 	output.ToImage(filename, itn, totalDistance, totalTime)
@@ -63,8 +68,10 @@ func BestInsertion() {
 
 	totalDistance := measure.TotalDistance(itn)
 	totalTime := measure.TimeSpent(itn)
+	totalFuelSpent := measure.SpentFuel(itn)
 	log.Println("BestiInsertion: Total Distance:", totalDistance)
 	log.Println("BestiInsertion: Total Time:", totalTime)
+	log.Println("BestiInsertion: Total Fuel Spent:", totalFuelSpent)
 
 	filename := "best-insertion.png"
 	output.ToImage(filename, itn, totalDistance, totalTime)
@@ -80,8 +87,10 @@ func BestInsertionWithDrones() {
 
 	totalDistance := measure.TotalDistance(itn)
 	totalTime := measure.TimeSpent(itn)
+	totalFuelSpent := measure.SpentFuel(itn)
 	log.Println("BestiInsertionWithDrones: Total Distance:", totalDistance)
 	log.Println("BestiInsertionWithDrones: Total Time:", totalTime)
+	log.Println("BestiInsertionWithDrones: Total Fuel Spent:", totalFuelSpent)
 
 	filename := "best-insertion-with-drones.png"
 	output.ToImage(filename, itn, totalDistance, totalTime)
@@ -97,9 +106,30 @@ func Covering() {
 
 	totalDistance := measure.TotalDistance(itn)
 	totalTime := measure.TimeSpent(itn)
+	totalFuelSpent := measure.SpentFuel(itn)
 	log.Println("Covering: Total Distance:", totalDistance)
 	log.Println("Covering: Total Time:", totalTime)
+	log.Println("Covering: Total Fuel Spent:", totalFuelSpent)
 
 	filename := "covering.png"
+	output.ToImage(filename, itn, totalDistance, totalTime)
+}
+
+func CoveringMaxDrones() {
+	initialPoint := gps.Point{Name: "initialPoint"}
+	car := vehicle.NewCar("car1", initialPoint)
+	car.NewDrone("drone1")
+	itn := itinerary.New(car)
+	neighorhoodDistance := vehicle.DroneRange / 2
+	csp.CoveringWithDrones([]itinerary.Itinerary{itn}, gps.GetMap(), neighorhoodDistance)
+
+	totalDistance := measure.TotalDistance(itn)
+	totalTime := measure.TimeSpent(itn)
+	totalFuelSpent := measure.SpentFuel(itn)
+	log.Println("CoveringMaxDrones: Total Distance:", totalDistance)
+	log.Println("CoveringMaxDrones: Total Time:", totalTime)
+	log.Println("CoveringMaxDrones: Total Fuel Spent:", totalFuelSpent)
+
+	filename := "covering-max-drones.png"
 	output.ToImage(filename, itn, totalDistance, totalTime)
 }
