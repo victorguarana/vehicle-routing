c97f151a74b51c8375ae748c285fce9ed8007746
Victor Guaraná <victorguarana@hotmail.com>
2024-05-23 22:38:54 -0300

    Implement map generator

---
+++ cmd/map_generator.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math/rand"
+	"os"
+)
+
+const filename = "map_full.txt"
+const depositQnt = 3
+const clientQnt = 100
+
+func main() {
+	file, err := os.Create(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	writer := bufio.NewWriter(file)
+	defer writer.Flush()
+
+	writeRandomWarehouses(writer)
+	writeRandomClients(writer)
+}
+
+func writeRandomWarehouses(writer *bufio.Writer) {
+	for i := 1; i <= depositQnt; i++ {
+		line := fmt.Sprintf("Deposito%d;%d;%d;%d\n", i, randomPosition(), randomPosition(), 0)
+		if _, err := writer.WriteString(line); err != nil {
+			panic(err)
+		}
+	}
+}
+
+func writeRandomClients(writer *bufio.Writer) {
+	for i := 1; i <= clientQnt; i++ {
+		line := fmt.Sprintf("Cliente%d;%d;%d;%d\n", i, randomPosition(), randomPosition(), randomPackage())
+		if _, err := writer.WriteString(line); err != nil {
+			panic(err)
+		}
+	}
+}
+
+func randomPackage() int {
+	return (rand.Int() % 13) + 1
+}
+
+func randomPosition() int {
+	return (rand.Int() % 100) - 50
+}
