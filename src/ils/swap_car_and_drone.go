5a285825c3ef09ce34e48bc7fded5a50824598a7
Victor Guaraná <victorguarana@hotmail.com>
2024-06-26 13:56:57 -0300

    Refactor swap car and drone

--- src/ils/swap_car_and_drone.go
+++ src/ils/swap_car_and_drone.go
@@ -2,7 +2,6 @@ package ils
 
 import (
 	"errors"
-	"log"
 
 	"github.com/victorguarana/vehicle-routing/src/itinerary"
 	"github.com/victorguarana/vehicle-routing/src/measure"
@@ -16,15 +15,7 @@ func SwapCarAndDrone(modifier itinerary.Modifier, finder itinerary.Finder) error
 	}
 
 	modifier.RemoveDroneStopFromFlight(worstDroneStopCost.Index, worstDroneStopCost.Flight)
-	var err error
-	for _, carStopCost := range swappableCarStopsOrdered {
-		if err = modifier.InsertDroneDelivery(carStopCost.Stop.Point(), measure.TotalDistance); err == nil {
-			log.Printf("Car stop %v was shifted to drone", carStopCost.Stop)
-			modifier.RemoveMainStopFromRoute(carStopCost.Index)
-			break
-		}
-	}
-	if err != nil {
+	if success := tryToInsertDroneDelivery(modifier, swappableCarStopsOrdered); !success {
 		return errors.New("No car stop can be inserted into flight")
 	}
 
@@ -33,4 +24,15 @@ func SwapCarAndDrone(modifier itinerary.Modifier, finder itinerary.Finder) error
 	}
 
 	return nil
+}
+
+func tryToInsertDroneDelivery(modifier itinerary.Modifier, swappableCarStopsOrdered []itinerary.CarStopCost) bool {
+	for _, carStopCost := range swappableCarStopsOrdered {
+		if err := modifier.InsertDroneDelivery(carStopCost.Stop.Point(), measure.TotalDistance); err == nil {
+			modifier.RemoveMainStopFromRoute(carStopCost.Index)
+			return true
+		}
+	}
+
+	return false
 }
