d2c736b30f2c345099d043fd41bc964f07804985
Victor Guaraná <victorguarana@hotmail.com>
2025-04-29 22:16:05 -0300

    Implements vehicle chooser by percentage

---
+++ internal/brkga/positiondecoder/vehicle_chooser_by_percentage.go
@@ -0,0 +1,89 @@
+package positiondecoder
+
+import (
+	"math"
+
+	"github.com/victorguarana/vehicle-routing/internal/brkga"
+	"github.com/victorguarana/vehicle-routing/internal/gps"
+	"github.com/victorguarana/vehicle-routing/internal/vehicle"
+)
+
+type vehicleChooserByPercentage struct {
+	gpsMap          gps.Map
+	dronePercentage float64
+	carPercentage   float64
+}
+
+func NewVehicleChooserByPercentage(gpsMap gps.Map, dronePercentage float64) *vehicleChooserByPercentage {
+	ch := &vehicleChooserByPercentage{
+		gpsMap:          gpsMap,
+		dronePercentage: dronePercentage,
+		carPercentage:   1 - dronePercentage,
+	}
+
+	return ch
+}
+
+func (c *vehicleChooserByPercentage) defineVehicle(carList []vehicle.ICar, chromossome *brkga.Chromossome) (vehicle.ICar, vehicle.IDrone) {
+	moduledGene := c.calcModuledGene(chromossome)
+
+	if moduledGene < c.carPercentage {
+		return c.defineCar(carList, moduledGene), nil
+	}
+
+	return c.defineDrone(carList, moduledGene)
+}
+
+func (c *vehicleChooserByPercentage) calcModuledGene(chromossome *brkga.Chromossome) float64 {
+	amplifiedGene := chromossome.Gene() * c.calcGeneAmplifier()
+	return math.Mod(amplifiedGene, c.calcGeneModule())
+}
+
+func (c *vehicleChooserByPercentage) calcGeneAmplifier() float64 {
+	return float64(len(c.gpsMap.Clients))
+}
+
+func (c *vehicleChooserByPercentage) calcGeneModule() float64 {
+	// Returns 1.0 because we need to use the percentage of the car and drone
+	return 1.0
+}
+
+func (c *vehicleChooserByPercentage) countDrones(carList []vehicle.ICar) int {
+	droneCount := 0
+	for _, car := range carList {
+		droneCount += len(car.Drones())
+	}
+	return droneCount
+}
+
+func (c *vehicleChooserByPercentage) defineDrone(carList []vehicle.ICar, moduledGene float64) (vehicle.ICar, vehicle.IDrone) {
+	droneCount := c.countDrones(carList)
+	percentageForEachDrone := c.dronePercentage / float64(droneCount)
+	dronePercentageSum := 0.0 + c.carPercentage
+
+	for _, car := range carList {
+		for _, drone := range car.Drones() {
+			dronePercentageSum += percentageForEachDrone
+			if dronePercentageSum >= moduledGene {
+				return car, drone
+			}
+		}
+	}
+
+	return nil, nil
+}
+
+func (c *vehicleChooserByPercentage) defineCar(carList []vehicle.ICar, moduledGene float64) vehicle.ICar {
+	carCount := len(carList)
+	percentageForEachCar := c.carPercentage / float64(carCount)
+	carPercentageSum := 0.0
+
+	for _, car := range carList {
+		carPercentageSum += percentageForEachCar
+		if carPercentageSum >= moduledGene {
+			return car
+		}
+	}
+
+	return nil
+}
---
+++ internal/brkga/positiondecoder/vehicle_chooser_by_percentage_test.go
@@ -0,0 +1,155 @@
+package positiondecoder
+
+import (
+	"math"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"github.com/victorguarana/vehicle-routing/internal/brkga"
+	"github.com/victorguarana/vehicle-routing/internal/gps"
+	"github.com/victorguarana/vehicle-routing/internal/vehicle"
+	mockvehicle "github.com/victorguarana/vehicle-routing/internal/vehicle/mock"
+	"go.uber.org/mock/gomock"
+)
+
+var _ = Describe("vehicleChooserByPercentage", func() {
+	var sut vehicleChooserByPercentage
+	var mockedCtrl *gomock.Controller
+	var mockedCar1 *mockvehicle.MockICar
+	var mockedCar2 *mockvehicle.MockICar
+	var mockedDrone1 *mockvehicle.MockIDrone
+	var mockedDrone2 *mockvehicle.MockIDrone
+
+	BeforeEach(func() {
+		mockedCtrl = gomock.NewController(GinkgoT())
+		mockedCar1 = mockvehicle.NewMockICar(mockedCtrl)
+		mockedCar2 = mockvehicle.NewMockICar(mockedCtrl)
+		mockedDrone1 = mockvehicle.NewMockIDrone(mockedCtrl)
+		mockedDrone2 = mockvehicle.NewMockIDrone(mockedCtrl)
+
+		sut = vehicleChooserByPercentage{}
+	})
+
+	Describe("NewVehicleChooserByPercentage", func() {
+		It("should create vehicleChooserByPercentage with correct params", func() {
+			gpsMap := gps.Map{
+				Clients: []gps.Point{
+					{Name: "client1"},
+					{Name: "client2"},
+					{Name: "client3"},
+				},
+			}
+
+			receivedVehicleChooser := NewVehicleChooserByPercentage(gpsMap, 0.6)
+			expectedVehicleChooser := &vehicleChooserByPercentage{
+				gpsMap:          gpsMap,
+				dronePercentage: 0.6,
+				carPercentage:   0.4,
+			}
+
+			Expect(receivedVehicleChooser).To(Equal(expectedVehicleChooser))
+		})
+	})
+
+	Describe("defineVehicle", func() {
+		Context("when the gene corresponds to a car", func() {
+			It("should return only car", func() {
+				sut.gpsMap.Clients = []gps.Point{{}, {}, {}}
+				sut.carPercentage = 0.7
+				carList := []vehicle.ICar{mockedCar1, mockedCar2}
+				c := brkga.Chromossome(0.1)
+				chromossome1 := &c
+
+				mockedCar1.EXPECT().Drones().Return([]vehicle.IDrone{}).AnyTimes()
+				mockedCar2.EXPECT().Drones().Return([]vehicle.IDrone{}).AnyTimes()
+
+				receivedCar, receivedDrone := sut.defineVehicle(carList, chromossome1)
+				Expect(receivedCar).To(Equal(mockedCar1))
+				Expect(receivedDrone).To(BeNil())
+			})
+		})
+
+		Context("when the gene corresponds to a drone", func() {
+			It("should return drone and car", func() {
+				sut.gpsMap.Clients = []gps.Point{{}, {}, {}}
+				sut.carPercentage = 0.4
+				sut.dronePercentage = 0.6
+				carList := []vehicle.ICar{mockedCar1, mockedCar2}
+				c := brkga.Chromossome(0.9)
+				chromossome1 := &c
+
+				mockedCar1.EXPECT().Drones().Return([]vehicle.IDrone{mockedDrone1}).AnyTimes()
+				mockedCar2.EXPECT().Drones().Return([]vehicle.IDrone{mockedDrone2}).AnyTimes()
+
+				receivedCar, receivedDrone := sut.defineVehicle(carList, chromossome1)
+				Expect(receivedCar).To(Equal(mockedCar2))
+				Expect(receivedDrone).To(Equal(mockedDrone2))
+			})
+		})
+	})
+
+	Describe("calcModuledGene", func() {
+		It("should calculate the moduled gene", func() {
+			sut.gpsMap.Clients = []gps.Point{{}, {}, {}, {}}
+			c := brkga.Chromossome(0.8)
+			chromossome1 := &c
+
+			receivedGene := sut.calcModuledGene(chromossome1)
+			roundedReceivedGene := math.Round(receivedGene*10) / 10
+			Expect(roundedReceivedGene).To(Equal(0.2))
+		})
+	})
+
+	Describe("calcGeneAmplifier", func() {
+		It("should calculate the gene amplifier", func() {
+			sut.gpsMap.Clients = []gps.Point{{}, {}, {}}
+			Expect(sut.calcGeneAmplifier()).To(Equal(3.0))
+		})
+	})
+
+	Describe("defineDrone", func() {
+		It("should return the first drone and car", func() {
+			sut.dronePercentage = 0.3
+			sut.carPercentage = 0.7
+			carList := []vehicle.ICar{mockedCar1, mockedCar2}
+
+			mockedCar1.EXPECT().Drones().Return([]vehicle.IDrone{mockedDrone1}).AnyTimes()
+			mockedCar2.EXPECT().Drones().Return([]vehicle.IDrone{mockedDrone2}).AnyTimes()
+
+			receivedCar, receivedDrone := sut.defineDrone(carList, 0.85)
+			Expect(receivedCar).To(BeIdenticalTo(mockedCar1))
+			Expect(receivedDrone).To(BeIdenticalTo(mockedDrone1))
+		})
+
+		It("should return the second drone and car", func() {
+			sut.dronePercentage = 0.3
+			sut.carPercentage = 0.7
+			carList := []vehicle.ICar{mockedCar1, mockedCar2}
+
+			mockedCar1.EXPECT().Drones().Return([]vehicle.IDrone{mockedDrone1}).AnyTimes()
+			mockedCar2.EXPECT().Drones().Return([]vehicle.IDrone{mockedDrone2}).AnyTimes()
+
+			receivedCar, receivedDrone := sut.defineDrone(carList, 1)
+			Expect(receivedCar).To(BeIdenticalTo(mockedCar2))
+			Expect(receivedDrone).To(BeIdenticalTo(mockedDrone2))
+		})
+	})
+
+	Describe("defineCar", func() {
+		It("should return the first car", func() {
+			sut.carPercentage = 0.4
+			carList := []vehicle.ICar{mockedCar1, mockedCar2}
+
+			receivedCar := sut.defineCar(carList, 0.2)
+			Expect(receivedCar).To(BeIdenticalTo(mockedCar1))
+		})
+
+		It("should return the second car", func() {
+			sut.carPercentage = 0.4
+			carList := []vehicle.ICar{mockedCar1, mockedCar2}
+
+			receivedCar := sut.defineCar(carList, 0.4)
+			Expect(receivedCar).To(BeIdenticalTo(mockedCar2))
+		})
+	})
+})
